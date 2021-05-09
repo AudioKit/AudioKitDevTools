0a3ad35cd8bc01343e41ae147ae4614c28cb9d17
Aurelius Prochazka <aurephd@gmail.com>
2021-05-09 11:57:57 -0700

    Updated scripts

--- bin/generate_generator_node.rb
+++ bin/generate_generator_node.rb
@@ -100,9 +100,7 @@ FileUtils.mkdir_p(output_folder) unless File.directory?(output_folder)
 ################
 # AKNode.swift #
 ################
-audio_unit_setup_partial = new_partial("audio_unit_setup")
 parameters_partial       = new_partial("parameters")
-internal_au_partial      = new_partial("internal_au")
 
 File.open("templates/AKGeneratorNode.swift.erb") { |template|
 	erb = ERB.new( template.read, nil, '-' )
